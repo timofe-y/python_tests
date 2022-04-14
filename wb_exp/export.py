ee37af7f9f8b11d95f59819bd7de5a18fa581344
timofe-y <103623042+timofe-y@users.noreply.github.com>
2022-04-14 15:50:30 +0700

    Update export.py

--- wb_exp/export.py
+++ wb_exp/export.py
@@ -8,8 +8,6 @@ import base64
 import global_vars
 import svg_to_image
 
-
-
 # Экспорт закза
 def export_supplie(A_supplie_lise):
     global_vars.main_logger.info('Start')
@@ -22,19 +20,10 @@ def export_supplie(A_supplie_lise):
     index = 0
     for item in A_supplie_lise:
         ws.write(index, 0, item['orderId'])
-        # ws.write(index, 1, item['barcode'])
         ws.write(index, 1, item['name'])
         ws.write(index, 2, item['article'])
-#        ws.write(index, 4, item['wbStickerSvgBase64'])
-
         sticker_image_file_name = global_vars.Export_catalog + item['article'] + '_' + str(item['orderId']) + '.jpeg'
         svg_to_image.create_image(base64.b64decode(item['wbStickerSvgBase64']), sticker_image_file_name)
         index = index + 1
     wb.save(order_file_name)
     global_vars.main_logger.info('{} orders exported'.format(index))
-
-
-
-
-
-
